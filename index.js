5034a4f24b36b014e76e9bf298b0c58f6df67bfc
Bright Jonathan <brightjonathan000@gmail.com>
2023-08-21 10:46:32 +0100

    slider image project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let slideIndex = 0;
 showSlides();
 
+
+
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -9,10 +11,14 @@ function showSlides() {
     slides[i].style.display = "none";  
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  };
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
-  }
+  };
+  
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
